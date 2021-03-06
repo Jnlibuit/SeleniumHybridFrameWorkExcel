d2f8d1825d9a252d205117f0fddaa7acd16c490b
nlibu <nlibu@10.0.0.12>
2021-03-06 10:33:40 -0500

    Commit

--- SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/sample/objectrepository/AccountPage.java
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/sample/objectrepository/AccountPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 
 public class AccountPage {
 	
-	public static By btnSignin = By.xpath("//div[@id='nav-signin-tooltip']/a/span");
-
+	
+public static By btnSignin = By.xpath("//header/div[@id='navbar']/div[@id='nav-flyout-anchor']/div[@id='nav-flyout-accountList']/div[2]/div[1]/div[1]/div[1]/a[1]/span[1]");
+public static By lnkSignout = By.xpath("//span[contains(text(),'Sign Out')]");
 }
--- SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/sample/reuse/CommonFunctions.java
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/sample/reuse/CommonFunctions.java
@@ -20,9 +20,11 @@ public class CommonFunctions {
 	public void Signin(String userName, String password) throws Exception
 	{
 		StartBrowser.childTest = StartBrowser.parentTest.createNode( "Signin to Amazon");
-		//aDriver.navigateToApplication("https://www.amazon.com/");
-		aDriver.click(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Link");
-		//aDriver.click(com.abc.applications.sample.objectrepository.AccountPage.btnSignin, "Sigin in button");	
+		
+		aDriver.navigateToApplication("https://www.amazon.com/");
+		aDriver.mouseHover(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Menu");
+		//aDriver.click(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Link");
+		aDriver.click(com.abc.applications.sample.objectrepository.AccountPage.btnSignin, "Sigin in button");	
 		aDriver.type(com.abc.applications.sample.objectrepository.SigninPage.txtEmaild, userName, "Email Id");
 		//aDriver.type(com.abc.applications.sample.objectrepository.SigninPage.txtEmaild, password, "Password");
 		aDriver.click(com.abc.applications.sample.objectrepository.SigninPage.btnContinue, "Continue button");
@@ -34,8 +36,20 @@ public class CommonFunctions {
 	public void AccountMenu() throws Exception
 	{
 		StartBrowser.childTest = StartBrowser.parentTest.createNode( "However to Account Menu");
+		aDriver.mouseHover(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Menu");
+		
+		
+			
+		
+		//Thread.sleep(1000);
+	}
+	
+	public void Signout() throws Exception
+	{
+		StartBrowser.childTest = StartBrowser.parentTest.createNode( "However to Account Menu");
 		
 		aDriver.mouseHover(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Menu");
+		aDriver.click(com.abc.applications.sample.objectrepository.AccountPage.lnkSignout, "Signout Link");
 		
 			
 		
