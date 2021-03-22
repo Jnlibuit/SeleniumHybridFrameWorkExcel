7abffefb92549c816cdf4e5cf2b6dace64b3c997
nlibu <nlibu@LAPTOP-2BGIP9DO>
2021-03-22 14:45:36 -0400

    Commit

--- SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/actions/I94Actions.java
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/actions/I94Actions.java
@@ -65,6 +65,62 @@ public class I94Actions {
 		//Thread.sleep(2000);
 	}
 
+	public void ApplyNowNilo(String firstName,String lastName,String birthDay,String birthMonth,String birthYear,String Gender,String documentNumber,String passportCountry) throws Exception
+	{
+		StartBrowser.childTest = StartBrowser.parentTest.createNode( "Apply Now");
+		aDriver.click(com.abc.applications.i94.or.I94.lnkAppForNewI94, "Apply For New I-94 Link");
+		aDriver.click(com.abc.applications.i94.or.I94.lnkApplyNow, "Apply Now");
+		aDriver.click(com.abc.applications.i94.or.I94.btnConsentContinue, "Consent & Continue");
+		aDriver.click(com.abc.applications.i94.or.I94.rbtnviaalandborder, "via a land border");
+		aDriver.type(com.abc.applications.i94.or.I94.txtFirstName, firstName, "First Name");
+		aDriver.type(com.abc.applications.i94.or.I94.txtLastName, lastName, "Last Name");
+		aDriver.type(com.abc.applications.i94.or.I94.txtBirthDay, birthDay, "Birth Day");
+		aDriver.type(com.abc.applications.i94.or.I94.txtBirthMo, birthMonth, "Birth Month");
+		aDriver.type(com.abc.applications.i94.or.I94.txtBirthYear, birthYear, "Birth Year");
+		//Gender(Gender);
+		System.out.println("+++++++++");
+		System.out.println(Gender);
+		String a = Gender;
+		System.out.println("aaaaaa");
+		System.out.println(a);
+		if (a == "M")
+		{
+			aDriver.click(com.abc.applications.i94.or.I94.rbtgendermale, "Gender Male");
+			System.out.println("This is M");
+		}else {
+			System.out.println("This is not M");
+		}
+		if (Gender == "F")
+		{
+			aDriver.click(com.abc.applications.i94.or.I94.rbtgendermale, "Gender Male");
+			System.out.println("This is F");
+		}
+		//aDriver.click(com.abc.applications.i94.or.I94.rbtgendermale, "Gender Male");
+		//aDriver.click(com.abc.applications.i94.or.I94.rbtgenderfemale, "Gender Female");
+		//aDriver.click(com.abc.applications.i94.or.I94.rbtgenderother, "Gender other");
+		aDriver.click(com.abc.applications.i94.or.I94.rbtpassport, "Passport");
+		aDriver.type(com.abc.applications.i94.or.I94.txtPassportNumber, documentNumber, "Document Number");
+		aDriver.type(com.abc.applications.i94.or.I94.txtPassportCountry, passportCountry, "Passport Country of Issue");
+		//aDriver.click(com.abc.applications.i94.or.I94.rbtbcc, "BCC");
+
+
+
+		/*
+		//aDriver.navigateToApplication("https://www.amazon.com/");
+		aDriver.mouseHover(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Menu");
+		//aDriver.click(com.abc.applications.sample.objectrepository.HomePage.lnkAccount, "Account Link");
+		aDriver.click(com.abc.applications.sample.objectrepository.AccountPage.btnSignin, "Sigin in button");	
+		aDriver.type(com.abc.applications.sample.objectrepository.SigninPage.txtEmaild, userName, "Email Id");
+		//aDriver.type(com.abc.applications.sample.objectrepository.SigninPage.txtEmaild, password, "Password");
+		aDriver.click(com.abc.applications.sample.objectrepository.SigninPage.btnContinue, "Continue button");
+		aDriver.type(com.abc.applications.sample.objectrepository.SigninPage.txtPassword, password, "Password");
+		aDriver.click(com.abc.applications.sample.objectrepository.SigninPage.btnSignIn, "Sign-In button");
+		 */
+
+
+		//Thread.sleep(2000);
+	}
+
 	public void Gender(String gender) throws Exception
 	{
 		System.out.println("******");
---
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94ApplyNowNilo.java
@@ -0,0 +1,50 @@
+package com.abc.applications.i94.scripts;
+
+import org.testng.annotations.Test;
+
+import com.abc.applications.i94.actions.I94Actions;
+import com.abc.config.StartBrowser;
+import com.abc.reuse.CommonFunctions;
+import com.codoid.products.fillo.Connection;
+import com.codoid.products.fillo.Fillo;
+import com.codoid.products.fillo.Recordset;
+
+public class I94ApplyNowNilo extends StartBrowser {
+	@Test
+	  public void testI94ApplyNow() throws Exception 
+	  {
+		Fillo g = new Fillo();
+		
+		Connection con = (Connection) g.getConnection("TestData/I94-File-Upload-Template_v0311.xlsx");
+		String strQuery = "Select * from Applicationstest";
+		Recordset recordset = con.executeQuery(strQuery);
+						
+		
+		 CommonFunctions cfs = new CommonFunctions();
+		  
+		 I94Actions i94a = new I94Actions ();
+		 
+		  while(recordset.next()){
+			  
+			  cfs.NavigateToHomePage("https://i94.cbp.dhs.gov/I94/#/home");
+				 
+			  i94a.ApplyNowNilo(recordset.getField("First Name"),recordset.getField("Last Name"),recordset.getField("Birth Day"),recordset.getField("Birth Month"),recordset.getField("Birth Year"),recordset.getField("Gender"),recordset.getField("Document Number"),recordset.getField("Passport Country of Issue"));
+			  
+			  
+			  //cfs.AccountMenu();
+			  
+			 Thread.sleep(9000);
+			
+		  }
+		  
+		  //cfs.NavigateToHomePage("https://i94.cbp.dhs.gov/I94/#/home");
+		 
+		  //i94a.ApplyNow();
+		  
+		  
+		  //cfs.AccountMenu();
+		  
+		 Thread.sleep(3000);
+	  }
+
+}
