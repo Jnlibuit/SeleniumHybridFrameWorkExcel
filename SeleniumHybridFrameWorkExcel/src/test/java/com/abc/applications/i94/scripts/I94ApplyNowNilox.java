c1205e83c548d6a794de172abed5450490f05742
nlibu <nlibu@10.0.0.12>
2021-03-24 11:24:18 -0400

    Commit

--- SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94ApplyNowNilox.java
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94ApplyNowNilox.java
@@ -9,15 +9,15 @@ import com.codoid.products.fillo.Connection;
 import com.codoid.products.fillo.Fillo;
 import com.codoid.products.fillo.Recordset;
 
-public class I94ApplyNowNilo extends StartBrowser {
+public class I94ApplyNowNilox extends StartBrowser {
 	@Test
 	  public void testI94ApplyNow() throws Exception 
 	  {
 		Fillo g = new Fillo();
 		
 		Connection con = (Connection) g.getConnection("TestData/I94-File-Upload-Template_v0311.xlsx");
-		//String strQuery = "Select * from Applicationstest";
-		String strQuery = "Select * from Applications";
+		String strQuery = "Select * from Applicationstest";
+		//String strQuery = "Select * from Applications";
 		Recordset recordset = con.executeQuery(strQuery);
 						
 		
