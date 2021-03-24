d60ad4c125bf0d459e521a002a8f8a367577eaea
nlibu <nlibu@10.0.0.12>
2021-03-24 16:36:53 -0400

    Commit

--- SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94ApplyNow.java
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94ApplyNow.java
@@ -11,43 +11,49 @@ import com.codoid.products.fillo.Recordset;
 
 public class I94ApplyNow extends StartBrowser {
 	@Test
-	  public void I94testApplyNow() throws Exception 
-	  {
-		Fillo g = new Fillo();
+
+	public void I94testApplyNow() throws Exception 
+	{
+		/**
+		 * Establish reference to Common Functions
+		 */
+		CommonFunctions cfs = new CommonFunctions();
 		
+		I94Actions i94a = new I94Actions ();
+		
+		/**
+		 * Get Config Properties
+		 */
+		String linkurl = cfs.processInput  ("/I94config");
+		
+		/**
+		 * Establish Excel File Connection
+		 */
+		
+		Fillo g = new Fillo();
+
 		Connection con = (Connection) g.getConnection("TestData/I94-File-Upload-Template_v0311.xlsx");
 		String strQuery = "Select * from Applicationstest";
 		Recordset recordset = con.executeQuery(strQuery);
-						
-		
-		 CommonFunctions cfs = new CommonFunctions();
-		  
-		 I94Actions i94a = new I94Actions ();
-		 String linkurl = cfs.processInput  ("/I94config");
-		 
-		  while(recordset.next()){
-			  
-			  cfs.NavigateToHomePage(linkurl);
-			 
-			  
-			  i94a.ApplyNow(recordset.getField("First Name"),recordset.getField("Last Name"),recordset.getField("Birth Date"),recordset.getField("Gender"),recordset.getField("Document Number"),recordset.getField("Passport Country of Issue"),recordset.getField("Passport Issue Date"),recordset.getField("Passport Expiration Date"),recordset.getField("Visa Country of Issuance"),recordset.getField("Visa Number"),recordset.getField("Visa Issue Date"));
-			  
-			  
-			  
-			  //cfs.AccountMenu();
-			  
-			 //Thread.sleep(9000);
-			
-		  }
-		  
-		  //cfs.NavigateToHomePage("https://i94.cbp.dhs.gov/I94/#/home");
-		 
-		  //i94a.ApplyNow();
-		  
-		  
-		  //cfs.AccountMenu();
-		  
-		 Thread.sleep(3000);
-	  }
+
+		/**
+		 * Test Script
+		 */
+		while(recordset.next()){
+
+			cfs.NavigateToHomePage(linkurl);
+
+
+			i94a.ApplyNow(recordset.getField("First Name"),recordset.getField("Last Name"),recordset.getField("Birth Date"),recordset.getField("Gender"),recordset.getField("Document Number"),recordset.getField("Passport Country of Issue"),recordset.getField("Passport Issue Date"),recordset.getField("Passport Expiration Date"),recordset.getField("Visa Country of Issuance"),recordset.getField("Visa Number"),recordset.getField("Visa Issue Date"));
+
+
+			//Thread.sleep(9000);
+
+		}
+
+
+
+		Thread.sleep(3000);
+	}
 
 }
--- SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94GroupUpload.java
+++ SeleniumHybridFrameWorkExcel/src/test/java/com/abc/applications/i94/scripts/I94GroupUpload.java
@@ -15,49 +15,44 @@ import com.codoid.products.fillo.Recordset;
 
 public class I94GroupUpload extends StartBrowser{
 	@Test
-	  public void I94testGroupUpload() throws Exception 
-	  {
-		
+	public void I94testGroupUpload() throws Exception 
+	{
+		/**
+		 * Establish reference to Common Functions
+		 */
+		CommonFunctions cfs = new CommonFunctions();
+
+		I94Actions i94a = new I94Actions ();
+
+		/**
+		 * Get Config Properties
+		 */
+		String linkurl = cfs.processInput  ("/I94config");
+
+		/**
+		 * Establish Excel File Connection
+		 */
 		Fillo g = new Fillo();
-		
+
 		Connection con = (Connection) g.getConnection("TestData/I94-File-Upload-Template_v0311.xlsx");
 		String strQuery = "Select * from Applicationstest";
 		Recordset recordset = con.executeQuery(strQuery);
-						
-		
-		 CommonFunctions cfs = new CommonFunctions();
-		  
-		 I94Actions i94a = new I94Actions ();
-		 String linkurl = cfs.processInput  ("/I94config");
-		 
-		  while(recordset.next()){
-			  
-			  cfs.NavigateToHomePage(linkurl);
-				 
-			  i94a.GroupUpload(recordset.getField("First Name"),recordset.getField("Last Name"),recordset.getField("Email Address"));
-				//Use of Robot class to upload file
 
-				
+		/**
+		 * Test Script
+		 */
+
+		while(recordset.next()){
+
+			cfs.NavigateToHomePage(linkurl);
+
+			i94a.GroupUpload(recordset.getField("First Name"),recordset.getField("Last Name"),recordset.getField("Email Address"));
 
 
-				
-				//System.out.println("********");
-				//System.out.println(filepath);
-				//System.out.println("********");
-				//Copy above path to clipboard
-							  
-			 //Thread.sleep(3000);
-			
-		  }
-		  
-		  //cfs.NavigateToHomePage("https://i94.cbp.dhs.gov/I94/#/home");
-		 
-		  //i94a.ApplyNow();
-		  
-		  
-		  //cfs.AccountMenu();
-		  
-		 Thread.sleep(3000);
-	  }
+		}
+
+
+		//Thread.sleep(3000);
+	}
 
 }
